bc5e201a52da792b94432249834a2ba60b57151a
Angela Laub <angela.laub@gmx.de>
2019-04-23 14:54:44 +0200

    prevent selecting of matched and open cards

--- js/app.js
+++ js/app.js
@@ -47,21 +47,15 @@ function shuffle(array) {
 }
 
 
-let shuffledCards = shuffle(cards);
-// console.log(shuffledCards);
+// let shuffledCards = shuffle(cards);
+let shuffledCards = cards; // unshuffled cards for testing purposes
 
 let deck = document.querySelector('.deck');
 
-/* 
-why doesn't this work: 
-deck.insertAdjacentHTML('beforeend', '<li class="card open show"><i class="fa $shuffledCards[i]"></i></li>')  
-OR: add li element, then add i element (appendChild?) ?
-" *   - loop through each card and create its HTML
- *   - add each card's HTML to the page" ...
-*/
     
+// doesn't work: deck.insertAdjacentHTML('beforeend', '<li class="card open show"><i class="fa $shuffledCards[i]"></i></li>')
 for(i = 0; i < 16; i++) {
-    deck.insertAdjacentHTML('beforeend', '<li class="card open show"><i class="fa ' +shuffledCards[i] +'"></i></li>')
+    deck.insertAdjacentHTML('beforeend', '<li class="card"><i class="fa ' +shuffledCards[i] +'"></i></li>')
 }
 
 /* card syntax: 
@@ -73,72 +67,105 @@ for(i = 0; i < 16; i++) {
 
 // toggle the clicked li-element's "open" and "show" classes ("turn" the card)
 // in case card is already matched, do nothing
-// TODO: es soll auch nichts passieren, wenn man auf die gleiche Karte 2x klickt...
 
-function turnCard(event) {
-    if (event.target.nodeName === 'LI' && !(event.target.classList.contains('match'))) {  
-        
-        
-        let card1 = event.target;
+let openCardCounter = 0;
 
-        // oder aus dieser funktion heraus jetzt 1) die Umdrehoptik und 2. das Matchen aufrufen?
-        
-        card1.classList.toggle('open');
-        card1.classList.toggle('show');
+function clickCard(event) {
+    // it's not possible to click an already matched or open card
+    if (event.target.nodeName === 'LI'&& !(event.target.classList.contains('match')) && !(event.target.classList.contains('open')) ) {
+
+        openCardCounter += 1;
+        // console.log(openCardCounter);
+        turnCard(event);
+        compareCard(event);
 
     }
 }
 
-// TODO: schauen, ob bereits eine Karte angeklickt wurde
-// Moment mal, die Cards SIND doch open, wieso wird der Code ausgeführt... ?
+// "turn" the card
+function turnCard(event) {
+    let card = event.target;
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+}
 
-//let cards = []; 
 let cardArray = [];
+let symbolArray = [];
+
 
 function compareCard(event) {
-    if (event.target.nodeName === 'LI' && !(event.target.classList.contains('match')) && !(event.target.classList.contains('open'))  ) {
+    // würde eigentlich in die äußere Funktion gehören...? 
+    // when 3rd card is clicked, turn first two cards over again and empty arrays of cards and symbols, then set counter back to 1
+    if(openCardCounter == 3) {
+        cardArray[0].classList.remove('open');
+        cardArray[0].classList.remove('show');
+        cardArray[1].classList.remove('open');
+        cardArray[1].classList.remove('show');
+        cardArray = [];
+        symbolArray = [];
+        openCardCounter = 1;
+    }
 
-        console.log(cardArray.length);
-
-        if (cardArray.length < 2) {
-            let card = event.target;
-            let cardSymbol = card.firstElementChild.classList[1];
-    
-            cardArray.push(cardSymbol);
-            console.log(cardArray);
-            }
-
-       console.log(cardArray.length);
-        }
-
-       
-        if (cardArray.length == 2) {  
-            if (cardArray[0] == cardArray[1]) {
-                console.log('match!');
-                }
-            }
-            
-
+    cardArray.push(event.target); // holds the element
+    symbolArray.push(event.target.firstElementChild.classList[1])  // holds the name of the symbol class
+ 
+    // check for matching symbols
+    if (openCardCounter == 2) {  
+        if (symbolArray[0] == symbolArray[1]) {
+            // console.log('match!');
+            cardArray[0].classList.add('match');
+            cardArray[1].classList.add('match');
+        } 
+    }
 }
 
 
-
 /*let deck = document.querySelector('.deck');*/ // code already exists above
-deck.addEventListener('click', turnCard);
+deck.addEventListener('click', clickCard);
 
-deck.addEventListener('click', compareCard);
+
+function restart() {
+
+    console.log('restart!');
+
+    /*
+    Alle Karten werden wieder umgedreht
+    Alle Arrays leeren
+    ClckCounter auf Null
+
+    Movecounter auf Null
+    Timer auf Null
+    Sterne auf Null
+
+
+    */
+}
+
+const restartButton = document.querySelector('.restart');
+console.log(restartButton);
+restartButton.addEventListener('click', restart);
+
+/* Spielende: 
+*   Counter stoppt
+* Popup mit Punkten
+*
+*
+*/
 
 
 
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- * 
- * 
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ * 
+ * 
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+ 
